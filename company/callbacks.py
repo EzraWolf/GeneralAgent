0b5a8df78c63d22f69525c09d1c4273d8175ee72
Ezra Wolf <wolfezraw@gmail.com>
2023-09-20 19:15:02 -0400

    Added more callbacks

--- company/callbacks.py
+++ company/callbacks.py
@@ -5,7 +5,7 @@ from general_agent.agent import callback
     "View the current project space, its structure, and what folders and files are in it",
     dict,
 )
-def get_workspace_dirs() -> dict:
+def view_workspace() -> dict:
     pass
 
 
@@ -21,6 +21,35 @@ def create_file(
     filetype: str,
     location: str,
 ) -> None:
+    pass
+
+
+@callback.new(
+    "Return a list of all possible agents",
+    list[str]
+)
+def get_all_agents() -> list[str]:
+    pass
+
+
+@callback.new(
+    "Talk and work with other agents. Returns their response",
+    str,
+    callback.new_arg("agent", str, "The agent you are trying to talk to", enum=get_all_agents())
+)
+def summon_agent(agent: str, query: str) -> str:
+    pass
+
+
+@callback.new(
+    "Get help from, or talk to a human. \
+This takes time and energy so use it sparingly. \
+You will always follow their response.",
+    str,
+    callback.new_arg("goal", str, "Your desired end result"),
+    callback.new_arg("text", str, "The question or thing you want to ask a human")
+)
+def talk_to_a_human(goal: str, text: str) -> str:
     pass
 
 
